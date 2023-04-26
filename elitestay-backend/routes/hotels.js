c92da7e3238ae2cff4e28c5f3262e8f1a28310e4
Abhijit Mali <maliabhijit183@gmail.com>
2023-04-26 16:16:19 +0530

    feat:added hotel api

--- elitestay-backend/routes/hotels.js
+++ elitestay-backend/routes/hotels.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
-const Hotel = require('../models/hotel')
+const Hotel = require('../models/hotel');
+const e = require("express");
+const { error } = require("console");
 const router = express.Router();
 
 // CREATE
@@ -18,9 +20,59 @@ router.post("/", async (req, res) => {
 
 // UPDATE
 
+router.put("/:id",async (req,res) => {
+  
+      try {
+        const updateHotel = await Hotel.findByIdAndUpdate(req.params.id , { $set : req.body} ,{ new: true });
+        res.status(200).json(updateHotel);
+      } catch ( error) {
+        res.status(404).json(error);
+      }
+
+});
+
 // DELETE
 
-// GET
+router.delete("/:id",async (req,res) => {
+  
+  try {
+    await Hotel.findByIdAndRemove(req.params.id);
+    res.status(200).json({message: "Hotel Has Been Deleted"});
+  } catch ( error) {
+    res.status(404).json(error);
+  }
+
+});
+
+// GET ALL 
+
+router.get("/:id" ,async (req,res) =>{
+
+  try {
+    const data = await Hotel.findById(req.params.id);
+    // console.log(" No Data ");
+    res.status(200).json(data);
+  }
+  catch(error){
+  res.status(400).json({message : "No Result Found"});
+  }
+});
+
+
 // GET All
+
+router.get("/" , async (req,res) =>{
+
+  try {
+    const hotels =  await Hotel.find();
+    // console.log(" No Data ");
+    
+    res.status(200).json(hotels);
+  }
+  catch(error){
+  res.status(400).json({message : "No Result Found"});
+  }
+});
+
 
 module.exports = router;
